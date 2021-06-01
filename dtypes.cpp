2a0f55d6d65cd427139d7c699d76d58e01f6aac4
Shreya-Mahajan <68501770+Shreya-Mahajan@users.noreply.github.com>
2021-06-01 20:36:53 +0530

    Add files via upload

---
+++ dtypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 4;
+    char b = 'r';
+    double c = 2.33;
+    float d = 3.44445566;
+    bool y = true;
+    cout << x << endl
+         << b << endl
+         << c << endl
+         << d << endl
+         << y;
+    return 0;
+}
